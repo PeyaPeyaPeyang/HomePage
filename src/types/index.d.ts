ac2afa512ca61d4376db3217bf78fa24e357453a
Peyang <peyang@peya.tokyo>
2023-03-12 20:19:34 +0900

    コンタクト計慮

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,11 +1,7 @@
-import type { validateEmail, submitContactForm } from "@/lib/contents/contact"
 import type { bakePassword, submitPassword, getRandomHint } from "@/lib/private/login"
 
 declare global {
     interface Window {
-        validateEmail: typeof validateEmail
-        submitContactForm: typeof submitContactForm
-
         bakePassword: typeof bakePassword
         submitPassword: typeof submitPassword
         getRandomHint: typeof getRandomHint
